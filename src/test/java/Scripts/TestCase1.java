e08d24e9a8fc6959847ae879ee30ead73d9a81cb
91903 <91903@DESKTOP-VF7SBGT>
2022-09-11 16:37:50 +0530

    hi

--- src/test/java/Scripts/TestCase1.java
+++ src/test/java/Scripts/TestCase1.java
@@ -1,5 +1,18 @@
 package Scripts;
 
-public class TestCase1 {
+import org.testng.annotations.Test;
+
+import genericLibrary.BaseClass;
+import pomPages.SkillaryLoginPage;
+
+public class TestCase1  extends BaseClass{
+	@Test
+	public void tc1() {
+		SkillaryLoginPage s = new SkillaryLoginPage(driver);
+		s.gearsbutton();
+		s.skillarydemoapplication();
+		
+		
+	}
 
 }
--- src/test/java/Scripts/TestCase2.java
+++ src/test/java/Scripts/TestCase2.java
@@ -1,7 +1,5 @@
 package Scripts;
 
-import java.io.IOException;
-
 import org.testng.annotations.Test;
 
 import genericLibrary.BaseClass;
@@ -10,14 +8,15 @@ import pomPages.SkillaryLoginPage;
 
 public class TestCase2 extends BaseClass {
 	@Test
-	public void tc2() throws IOException {
+	public void tc2() {
 		SkillaryLoginPage s = new SkillaryLoginPage(driver);
 		s.gearsbutton();
 		s.skillarydemoapplication();
 		
 		SkillaryDemoLoginPage sd = new SkillaryDemoLoginPage(driver);
-		utilies.switchingtabs(driver);
+		
 	}
+	
 	
 
 }
